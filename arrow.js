5cc99e63223acb2b80a1731eb2221ffd4b3a8be0
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-18 12:55:58 +0600

    sum arrow function

--- arrow.js
+++ arrow.js
@@ -6,4 +6,10 @@
     const sayGreat=(name)=>{
         return`Hello ${name},Good Morning.`
     }
-    console.log(sayGreat("manik"))
+    // console.log(sayGreat("manik"))
+    const multiply=(a,b)=>{
+        const sum=a+b;
+        const multiply=sum*3;
+        return multiply;
+    }
+    console.log(multiply(1,2))
