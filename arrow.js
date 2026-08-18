763f143156e765f68a3df914bbde1fbb9385853f
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-18 12:52:35 +0600

    arrow function

---
+++ arrow.js
@@ -0,0 +1,9 @@
+// function sayGreet(name){
+//     return `Hello ${name},Good Morning`
+//     }
+//     console.log(sayGreet("rakib"))
+
+    const sayGreat=(name)=>{
+        return`Hello ${name},Good Morning.`
+    }
+    console.log(sayGreat("manik"))
--- spread.js
+++ spread.js
@@ -1,3 +1,15 @@
 let numbers=[10,45,94,93,645]
 let maxNumber=Math.max(...numbers);
 console.log(maxNumber)
+
+console.log(... numbers,1000)
+
+
+
+
+//Rest Oprator
+
+function sum(a,b,c,){
+    return a+b+c;
+}
+console.log(sum(10,20,30))
