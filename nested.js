c1356595116cd330b2dce6c010ff45cc53d52ad4
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-19 08:03:55 +0600

    nested looping

---
+++ nested.js
@@ -0,0 +1,11 @@
+// const { use } = require("react")
+
+const user={
+    name:"rakib",
+    age:35,
+    something:[20,30,40,50],
+    company:{
+        name:"programming hero",
+    }
+}
+console.log(user.something)
